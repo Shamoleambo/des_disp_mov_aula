78d3a39a83b3c7273fbc6bfb994c44febbbe45f1
Tiago Gomes <tidgomes@gmail.com>
2026-03-13 20:03:30 -0300

    find out how the constructor works, I don't HAVE to initialize stuff

--- src/aula_02/my_stuff/model/heranca/Todo.kt
+++ src/aula_02/my_stuff/model/heranca/Todo.kt
@@ -2,17 +2,7 @@ package aula_02.my_stuff.model.heranca
 
 import java.time.LocalDateTime
 
-open class Todo() {
-
-    var noteText: String = ""
-    var status: Boolean = false
-    lateinit var date: LocalDateTime
-
-    constructor(noteText: String, status: Boolean) : this() {
-        this.noteText = noteText
-        this.status = status
-        this.date = LocalDateTime.now()
-    }
+open class Todo(val noteText: String, val status: Boolean, val date: LocalDateTime) {
 
     override fun toString(): String {
         return "Note: $noteText"
