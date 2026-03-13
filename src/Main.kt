e4417042ebef92ba666e3172bd240086bfcc773b
Tiago Gomes <tidgomes@gmail.com>
2026-03-13 20:20:54 -0300

    mend

--- src/Main.kt
+++ src/Main.kt
@@ -2,8 +2,6 @@ import aula_02.my_stuff.model.heranca.Todo
 import aula_02.my_stuff.model.heranca.TodoDifficulty
 import java.time.LocalDateTime
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
     val todo = Todo("texto", false, LocalDateTime.now().plusDays(1))
